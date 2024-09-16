59a3cd4935b064eb85f3f1b38c3e1a53a4912aa4
Bruno <bganontha@gmail.com>
2024-09-16 15:08:52 +0100

    Task manager

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,16 @@
+#task reminder
+
+task = input("Enter your task: ")
+priority = input("Priority (high/medium/low): ")
+time_bound = input("Is it time-bound? (yes/no): ")
+
+#match cases
+match priority:
+    case "high":
+        print(f"Reminder: {task} is a {priority} priority task that requires immediate attention today!")
+    case "low":
+        print(f"Note: {task} is a {priority} priority task. Consider completing it when you have free time.")
+    case "medium":
+        print(f"Reminder: {task} is a {priority} priority task. Consider completing it when you have free time")
+    case _:
+        print("Invalid input. Please enter yes or no.")
