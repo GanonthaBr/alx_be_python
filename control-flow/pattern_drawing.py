381968820c6c3c179a83671581146cdef34b921e
Bruno <bganontha@gmail.com>
2024-09-16 14:43:36 +0100

    Fix bugs in the code

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,8 +1,9 @@
 # Utilize while loops and nested for loops to draw a simple text-based pattern
-row = int(input("Enter the size of the pattern: "))
+size = int(input("Enter the size of the pattern: "))
+row = size
 
-while(row):
-    for i in range(row):
+while row > 0:
+    for i in range(size):
         print("*", end=" ")
     print("")
     row-=1
