dd5eb425e774a7cfd4790e7bc543a63c0a5d9d22
Bruno <bganontha@gmail.com>
2024-09-23 10:05:13 +0100

    Refactor code: rename variables

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,12 +1,12 @@
-def perform_operation(num1,num2,operation):
+def perform_operation(num1:float,num2:float,operation:str):
     match operation:
-        case '+':
+        case 'add':
             return num1 + num2
-        case '-':
+        case 'subtract':
             return num1 - num2
-        case '*':
+        case 'multiply':
             return num1 * num2
-        case '/':
+        case 'divide':
             if num2 == 0:
                 return "Division by zero is not allowed"
             return num1 / num2
@@ -14,3 +14,4 @@ def perform_operation(num1,num2,operation):
             return "Invalid operation"
         
 
+print(perform_operation(1,2,'divide'))
